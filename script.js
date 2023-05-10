b479a022ab40959e7a2ea00bc5754132e4614bbd
kmfox1087 <kmfox1087@gmail.com>
2023-05-09 22:38:01 -0400

    Added webpage title and js function to fetch apiurl

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
 localStorage.clear();
 
-var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=eedf2a8246eeeb25f8bfcceff39ff8c5";
+function findCity() {
+    var cityName = titleCase($("#cityName")[0].value.trim());
+
+    var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid= eedf2a8246eeeb25f8bfcceff39ff8c5  ";
+
+    fetch(apiURL).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+
+                $("#city-name")[0].textContent = cityName + " (" + moment().format('M/D/YYYY') + ")";
+
+                $("#city-list").append('<button type="button" class="list-group-item list-group-item-light list-group-item-action city-name">' + cityName);
+
+                const lat = data.coord.lat;
+                const lon = data.coord.lon;
+
+                var latLonPair = lat.toString() + " " + lon.toString();
+
+                localStorage.setItem(cityName, latLonPair);
+
+                apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&units=imperial&appid= eedf2a8246eeeb25f8bfcceff39ff8c5  ";
+
+                fetch(apiURL).then(function (newResponse) {
+                    if (newResponse.ok) {
+                        newResponse.json().then(function (newData) {
+                            getCurrentWeather(newData);
+                        })
+                    }
+                })
+            })
+        } else {
+            alert("Cannot find city!");
+        }
+    })
+}
